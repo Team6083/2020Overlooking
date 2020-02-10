4be5396bdca135984dea5637cde7773b1bf61138
EricTasi <a0905060071@gmail.com>
2020-02-10 14:50:24 +0800

    add visiontracking system
    add visiontracking system

---
+++ src/main/java/frc/system/VisionTracking.java
@@ -0,0 +1,123 @@
+package frc.system;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.controller.PIDController;
+
+public class VisionTracking{
+    private boolean m_LimelightHasValidTarget = false;
+    private double m_LimelightDriveCommand = 0.0;
+    private double m_LimelightSteerCommand = 0.0;
+    private double steering_adjust = 0.0;
+
+    private PIDController PID_controller;
+
+    final double DESIRED_TARGET_Y_AXIS = 0.47639;        // Area of the target when the robot reaches the wall
+    final double MAX_DRIVE = 0.5;                   // Simple speed limit so we don't drive too fast
+    final double MAX_STEER = 0.5;  
+
+    public void Update_Limelight_Tracking()
+    {
+
+            double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+            double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+            double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+            //double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+
+            if (tv < 1.0)
+            {
+            m_LimelightHasValidTarget = false;
+            m_LimelightDriveCommand = 0.0;
+            m_LimelightSteerCommand = 0.0;
+            return;
+            }
+
+            m_LimelightHasValidTarget = true;
+
+            //wpilib function use to adjust the robot to aiming target
+            steering_adjust = -PID_controller.calculate(tx, 0.0);
+
+            //use MAX_DRIVE to limit robot turning speed
+            if(steering_adjust > 0){
+            if (steering_adjust > MAX_DRIVE)
+            {
+                steering_adjust = MAX_STEER;
+            }
+            }
+            else if (steering_adjust < -MAX_DRIVE){
+                steering_adjust = -MAX_STEER;
+            }
+
+            double drive_cmd = -PID_controller.calculate(ty,DESIRED_TARGET_Y_AXIS );
+
+            //use MAX_DRIVE to limit robot forward speed
+            if(drive_cmd > 0){
+            if (drive_cmd > MAX_DRIVE)
+            {
+                drive_cmd = MAX_DRIVE;
+            }
+            }
+            else if(drive_cmd < -MAX_DRIVE){
+                drive_cmd = -MAX_DRIVE;
+            }
+
+            m_LimelightSteerCommand = steering_adjust;
+            m_LimelightDriveCommand = drive_cmd;
+
+    }
+
+    public void seeking(){
+        double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+
+        if (tv == 0.0)
+        {
+          // We don't see the target, seek for the target by spinning in place at a safe speed.
+          steering_adjust = 0.3;
+          m_LimelightDriveCommand = 0.0;
+        }
+        else
+        {
+          Update_Limelight_Tracking();
+        }
+        
+        m_LimelightSteerCommand = steering_adjust;
+      }
+
+      public void setLEDMode(int ModeNumber){
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ModeNumber);
+      }
+
+      public void setCamMode(int CamNumber){
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(CamNumber);
+      }
+    
+      public double getValidTarget(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+      }
+    
+      public double getHorizontalOffset(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+      }
+    
+      public double getVerticalOffset(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+      }
+    
+      public double getTargetArea(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+      }
+    
+      /**
+       * 
+       * @return Skew or rotation (-90 degrees to 0 degrees)
+       */
+      public double getRotation(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ts").getDouble(0);
+      }
+    
+      /**
+       *  The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.
+       */
+      public double getLatencyContribution(){
+        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tl").getDouble(0);
+      }  
+}

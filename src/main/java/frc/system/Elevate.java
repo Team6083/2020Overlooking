362b5e45fbdec7c4ab84aee30991fa4558afd1b0
Leafoliage <leafoliages@gmail.com>
2020-02-12 11:46:14 +0800

    modify elevate

--- src/main/java/frc/system/Elevate.java
+++ src/main/java/frc/system/Elevate.java
@@ -1,5 +1,6 @@
 package frc.system;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 import org.team6083.lib.util.XBoxController;
 
@@ -7,23 +8,29 @@ public class Elevate {
     private static VictorSP Motor1;
     private static VictorSP Motor2;
     private static XBoxController xbox;
+    private static Timer time;
 
-    public static void init(XBoxController joy){
+    public static void init(final XBoxController joy) {
         Motor1 = new VictorSP(5);
         Motor2 = new VictorSP(6);
         xbox = joy;
+        time = new Timer();
     }
 
-    public static void teleop(){
-        if(xbox.getRawButton(3)){
-            Motor1.set(0.5);
-            Motor2.set(0.5);
-        } else if(xbox.getRawButton(4)) {
-            Motor1.set(-0.5);
-            Motor2.set(-0.5);
+    public static void teleop() {
+        if (xbox.getRawButtonPressed(3)) {
+            RunNSec(Motor1, 3);
+            RunNSec(Motor2, 3);
+        }
+    }
+
+    public static void RunNSec(VictorSP motor, int n) {
+        time.reset();
+        time.start();
+        if (time.get() < n) {
+            motor.set(0.5);
         } else {
-            Motor1.set(0);
-            Motor2.set(0);
+            motor.set(0);
         }
     }
 }

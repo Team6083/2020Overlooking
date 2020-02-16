9f36bb9c7e739d53d7f2db0e7408abc2511d8188
Leafoliage <leafoliages@gmail.com>
2020-02-16 13:16:03 +0800

    modify suck sent

--- src/main/java/frc/system/SuckSent.java
+++ src/main/java/frc/system/SuckSent.java
@@ -4,59 +4,54 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.Encoder;
+import org.team6083.lib.RobotPower;
+
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 public class SuckSent {
     private static WPI_VictorSPX suck;
     private static TalonSRX sent;
+    private static RobotPower power;
 
-
-    private static AnalogPotentiometer analogPotentiometer;
-    private static Encoder encoder;
-    private static double distanceWantBallToMove = 10;//this variable need to be tune
+    private static AnalogInput analogInput;
+    private static double distanceWantBallToMove = 10;// this variable need to be tune
+    private static int analogDistance = 35;
 
     public static void init() {
-        analogPotentiometer = new AnalogPotentiometer(0, 180, 30);
-        encoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+        RobotPower.init(0);
+
+        analogInput = new AnalogInput(0);
         suck = new WPI_VictorSPX(9);
         sent = new TalonSRX(10);
+        power = new RobotPower(12);
     }
 
     public static void teleop() {
-        encoder.setDistancePerPulse(0.15);//this parameter need to be measured
 
-        if(analogPotentiometer.get() > 35){
+        if (analogInput.getValue() > analogDistance) {
             sent.set(ControlMode.PercentOutput, 0.3);
-            if(encoder.getDistance() >= distanceWantBallToMove){
-                sent.set(ControlMode.PercentOutput, 0);
-          }
         }
-        
-        if(Robot.xbox.getYButton()){
-            suck.set(ControlMode.PercentOutput, 0.5);
-        }
-        else{
-            suck.set(ControlMode.PercentOutput,0);
-        }
-        
 
-        if(Robot.xbox.getRawButton(6)){
-            sent.set(ControlMode.PercentOutput, 0.4);
-        }
-        else{
+        if (sent.getSensorCollection().getPulseWidthPosition() >= distanceWantBallToMove) {
             sent.set(ControlMode.PercentOutput, 0);
         }
 
-        SmartDashboard.putNumber("Total distance",encoder.getDistance());
-        SmartDashboard.putNumber("Anolog of Potentiometer", analogPotentiometer.get());
+        if (Robot.xbox.getYButton()) {
+            suck.set(ControlMode.PercentOutput, 0.5);
+        } else {
+            suck.set(ControlMode.PercentOutput, 0);
+        }
 
+        SmartDashboard.putNumber("shoot motor voltage", power.getPortCurrent());
 
-        /*if(analog.getValue() > analogLenth) {
-            sent.set(ControlMode.PercentOutput, 0.5);
-        }*/
+        SmartDashboard.putNumber("Total distance", sent.getSensorCollection().getPulseWidthPosition());
+
+        /*
+         * if(analog.getValue() > analogLenth) { sent.set(ControlMode.PercentOutput,
+         * 0.5); }
+         */
 
         // analog.getValue();
         // analog.getVoltage();

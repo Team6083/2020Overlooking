5a8def62734106d9d386e74cc2b112b1b3f5777c
Leafoliage <leafoliages@gmail.com>
2020-02-22 20:54:51 +0800

    modify FacePort

--- src/main/java/frc/system/auto/FacePort.java
+++ src/main/java/frc/system/auto/FacePort.java
@@ -3,18 +3,18 @@ package frc.system.auto;
 import frc.system.Shooting;
 import frc.system.VisionTracking;
 
-public class FacePort extends AutoEngine{
-    //private static int errAngle = 5;
+public class FacePort extends AutoEngine {
+    // private static int errAngle = 5;
 
-    public static void loop(){
-        switch(step){
+    public static void loop() {
+        switch (step) {
             case 0:
                 /* take aim */
                 VisionTracking.seeking();
                 break;
             case 1:
                 /* shoot */
-                Shooting.shoot(4);  // value needs to be tuned
+                Shooting.shoot(4); // value needs to be tuned
                 break;
         }
     }

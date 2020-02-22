6820f765f8093d9cde7121f5cbd90a88fa6476e6
Leafoliage <leafoliages@gmail.com>
2020-02-22 20:23:58 +0800

    rename Step.java to StartLine.java

---
+++ src/main/java/frc/system/auto/StartLine.java
@@ -0,0 +1,13 @@
+package frc.system.auto;
+
+public class StartLine extends AutoEngine {
+    private static final int errAngle = 5;
+
+    public static void loop() {
+        switch (step) {
+            case 0:
+                
+                break;
+        }
+    }
+}
